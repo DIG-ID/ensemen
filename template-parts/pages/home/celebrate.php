fb7256d40a0ced90dfc53942b2282e4ac9285fce
Daniel-Digid <daniel@dig.id>
2026-04-28 16:51:31 +0100

    celebrate

--- template-parts/pages/home/celebrate.php
+++ template-parts/pages/home/celebrate.php
@@ -8,49 +8,64 @@
  */
 
 ?>
-<section id="section-celebrate" class="section-celebrate bg-brown pt-[50px] pb-[50px] md:pt-[100px] md:pb-[100px] xl:pt-52 xl:pb-52">
+<section id="section-celebrate" class="section-celebrate bg-brown pt-12 pb-12 md:pt-24 md:pb-24 xl:pt-52 xl:pb-52">
 	<div class="theme-container">
 		<div class="theme-grid gap-y-[50px] md:gap-y-0 items-start">
 
-			<div class="col-span-2 md:col-span-6 xl:col-start-3 xl:col-span-5">
-				<h2 class="title-main italic text-off-white"><?php the_field( 'celebrate_title' ); ?></h2>
+			<div class="col-span-2 md:col-span-6 block xl:hidden">
+				<h2 class="title-main italic text-off-white xl:pb-16"><?php the_field( 'celebrate_title' ); ?></h2>
 			</div>
 
-			<div class="col-span-2 md:col-start-4 md:col-span-3 md:pt-[75px] xl:col-start-8 xl:col-span-5 xl:pt-0">
+			<div class="col-span-2 block md:hidden">
 				<?php if ( get_field( 'celebrate_image_right' ) ) : ?>
 					<?php echo wp_get_attachment_image(
 						get_field( 'celebrate_image_right' ),
 						'full',
 						false,
 						array(
-							'class'         => 'celebrate_image_right w-full h-auto',
+							'class' => 'celebrate_image_right w-full h-auto',
 						)
 					); ?>
 				<?php endif; ?>
 			</div>
 
-			<div class="col-span-1 md:col-start-1 md:col-span-3 xl:col-start-1 xl:col-span-6 xl:-mt-32">
+			<div class="col-span-2 md:col-span-3 xl:col-span-7 pt-0 xl:pr-10">
+				<div class="hidden xl:grid xl:grid-cols-7">
+					<h2 class="xl:col-span-7 2xl:col-span-5 col-start-1 2xl:col-start-3 title-main italic text-off-white xl:pb-16"><?php the_field( 'celebrate_title' ); ?></h2>
+				</div>
+
 				<?php if ( get_field( 'celebrate_image_left' ) ) : ?>
 					<?php echo wp_get_attachment_image(
 						get_field( 'celebrate_image_left' ),
 						'full',
 						false,
 						array(
-							'class'         => 'celebrate_image_left w-full h-min-[471px] md:h-min-[471px] xl:h-min-none xl:h-auto',
+							'class' => 'celebrate_image_left w-full xl:h-auto mt-0 md:mt-24 xl:mt-0',
 						)
 					); ?>
 				<?php endif; ?>
 			</div>
 
-			<div class="col-span-1 md:col-start-4 md:col-span-3 md:pt-10 xl:col-start-7 xl:col-span-6 xl:pt-60">
+			<div class="col-span-2 md:col-span-3 xl:col-span-5">
+				<?php if ( get_field( 'celebrate_image_right' ) ) : ?>
+					<?php echo wp_get_attachment_image(
+						get_field( 'celebrate_image_right' ),
+						'full',
+						false,
+						array(
+							'class' => 'celebrate_image_right w-full h-auto hidden md:block',
+						)
+					); ?>
+				<?php endif; ?>
+
 				<?php if ( have_rows( 'celebrate_items' ) ) : ?>
-					<ul class="celebrate-items flex flex-col gap-[30px] xl:flex-row xl:gap-[109px]">
+					<ul class="celebrate-items flex flex-col xl:flex-row mt-8 md:mt-[4.65rem] xl:mt-32 2xl:mt-40">
 						<?php while ( have_rows( 'celebrate_items' ) ) : the_row(); ?>
 							<?php
 							$item_icon = get_sub_field( 'icon' );
 							$item_text = get_sub_field( 'text' );
 							?>
-							<li class="celebrate-items__item flex items-center gap-[19px] md:gap-[36px]">
+							<li class="celebrate-items__item flex flex-row xl:flex-col items-start mb-10 xl:mb-0 gap-9 xl:gap-4 flex-1">
 								<?php if ( $item_icon ) : ?>
 									<?php
 									echo wp_get_attachment_image(
@@ -58,8 +73,7 @@
 										'full',
 										false,
 										array(
-											'class'   => 'celebrate-items__icon shrink-0',
-											'loading' => 'lazy',
+											'class' => 'celebrate-items__icon shrink-0',
 										)
 									);
 									?>
@@ -71,13 +85,9 @@
 						<?php endwhile; ?>
 					</ul>
 				<?php endif; ?>
-			</div>
 
-			<div class="col-span-2 md:col-start-4 md:col-span-3 md:pt-10 xl:col-start-7 xl:col-span-4 xl:pt-16">
-				<p class="text-off-white font-openSans text-[18px] leading-[30px] tracking-[0.5px] max-w-[594px]"><?php the_field( 'celebrate_description' ); ?></p>
-			</div>
+				<p class="text-off-white font-openSans text-[18px] leading-[30px] tracking-[0.5px] xl:max-w-[594px] mt-1 xl:mt-10 mb-11 xl:mb-16"><?php the_field( 'celebrate_description' ); ?></p>
 
-			<div class="col-span-2 md:col-start-4 md:col-span-3 md:pt-10 xl:col-start-7 xl:col-span-2 pt-16">
 				<?php
 				$btn_primary = get_field( 'celebrate_button' );
 				if ( $btn_primary ) :
@@ -90,6 +100,7 @@
 					</a>
 				<?php endif; ?>
 			</div>
+
 
 		</div>
 	</div>
