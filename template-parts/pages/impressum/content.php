992363a4767004e081b36c12966466cf90794f69
bankz91 <brunoduarte.91300@gmail.com>
2026-04-29 11:26:55 +0100

    1

--- template-parts/pages/impressum/content.php
+++ template-parts/pages/impressum/content.php
@@ -7,9 +7,6 @@
  * @since 1.0.0
  */
 
-$inhaber = get_field( 'impressum_inhaber' );
-$contact = get_field( 'impressum_contact' );
-$credits = get_field( 'impressum_credits' );
 ?>
 <section id="section-impressum" class="section-impressum bg-brown pt-12 pb-20 md:pt-20 md:pb-32 xl:pt-32 xl:pb-48">
 	<div class="theme-container">
@@ -18,31 +15,25 @@ $credits = get_field( 'impressum_credits' );
 				<?php the_title(); ?>
 			</h1>
 
-			<?php if ( $inhaber ) : ?>
-				<div class="impressum-block col-start-1 col-span-1 md:col-start-2 md:col-span-2 xl:col-start-6 xl:col-span-2">
-					<h2 class="impressum-block__title"><?php echo $inhaber['title']; ?></h2>
-					<div class="impressum-block__content"><?php echo $inhaber['content']; ?></div>
-				</div>
-			<?php endif; ?>
+			<div class="impressum-block col-start-1 col-span-1 md:col-start-2 md:col-span-2 xl:col-start-6 xl:col-span-2">
+				<h2 class="impressum-block__title"><?php the_field( 'impressum_inhaber_title' ); ?></h2>
+				<div class="impressum-block__content"><?php the_field( 'impressum_inhaber_content' ); ?></div>
+			</div>
 
-			<?php if ( $contact ) : ?>
-				<div class="impressum-block col-start-2 col-span-1 md:col-start-4 md:col-span-3 xl:col-start-8 xl:col-span-5">
-					<h2 class="impressum-block__title"><?php echo $contact['phone_title']; ?></h2>
-					<p class="impressum-block__value"><?php echo $contact['phone_value']; ?></p>
+			<div class="impressum-block col-start-2 col-span-1 md:col-start-4 md:col-span-3 xl:col-start-8 xl:col-span-5">
+				<h2 class="impressum-block__title"><?php the_field( 'impressum_phone_title' ); ?></h2>
+				<p class="impressum-block__value"><?php the_field( 'impressum_phone_value' ); ?></p>
 
-					<h2 class="impressum-block__title impressum-block__title--spaced"><?php echo $contact['email_title']; ?></h2>
-					<p class="impressum-block__value">
-						<a href="mailto:<?php echo $contact['email_value']; ?>"><?php echo $contact['email_value']; ?></a>
-					</p>
-				</div>
-			<?php endif; ?>
+				<h2 class="impressum-block__title impressum-block__title--spaced"><?php the_field( 'impressum_email_title' ); ?></h2>
+				<p class="impressum-block__value">
+					<a href="mailto:<?php the_field( 'impressum_email_value' ); ?>"><?php the_field( 'impressum_email_value' ); ?></a>
+				</p>
+			</div>
 
-			<?php if ( $credits ) : ?>
-				<div class="impressum-block col-start-1 col-span-2 md:col-start-2 md:col-span-5 xl:col-start-6 xl:col-span-7">
-					<h2 class="impressum-block__title"><?php echo $credits['title']; ?></h2>
-					<div class="impressum-block__content"><?php echo $credits['content']; ?></div>
-				</div>
-			<?php endif; ?>
+			<div class="impressum-block col-start-1 col-span-2 md:col-start-2 md:col-span-5 xl:col-start-6 xl:col-span-7">
+				<h2 class="impressum-block__title"><?php the_field( 'impressum_credits_title' ); ?></h2>
+				<div class="impressum-block__content"><?php the_field( 'impressum_credits_content' ); ?></div>
+			</div>
 		</div>
 	</div>
 </section>
