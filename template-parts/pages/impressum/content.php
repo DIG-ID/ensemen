790f11bbcde0cf4c63e5b495a0c48f8b298933b3
bankz91 <brunoduarte.91300@gmail.com>
2026-04-29 11:35:52 +0100

    1

--- template-parts/pages/impressum/content.php
+++ template-parts/pages/impressum/content.php
@@ -10,8 +10,8 @@
 ?>
 <section id="section-impressum" class="section-impressum bg-brown pt-12 pb-20 md:pt-20 md:pb-32 xl:pt-32 xl:pb-48">
 	<div class="theme-container">
-		<div class="theme-grid gap-y-12 md:gap-y-16 xl:gap-y-20">
-			<h1 class="section-impressum__title col-start-1 col-span-2 md:col-start-1 md:col-span-6 xl:col-start-3 xl:col-span-10 mb-12 md:mb-16 xl:mb-24">
+		<div class="theme-grid">
+			<h1 class="section-impressum__title col-start-1 col-span-2 md:col-start-1 md:col-span-6 xl:col-start-3 xl:col-span-10 pb-32 md:pb-16 xl:pb-10">
 				<?php the_title(); ?>
 			</h1>
 
@@ -20,7 +20,7 @@
 				<div class="impressum-block__content"><?php the_field( 'inhaber_content' ); ?></div>
 			</div>
 
-			<div class="impressum-block col-start-2 col-span-1 md:col-start-4 md:col-span-3 xl:col-start-8 xl:col-span-5">
+			<div class="impressum-block col-start-2 col-span-1 md:col-start-4 md:col-span-3 xl:col-start-9 xl:col-span-5">
 				<h2 class="impressum-block__title"><?php the_field( 'contact_phone_title' ); ?></h2>
 				<p class="impressum-block__value"><?php the_field( 'contact_phone_value' ); ?></p>
 
@@ -30,7 +30,7 @@
 				</p>
 			</div>
 
-			<div class="impressum-block col-start-1 col-span-2 md:col-start-2 md:col-span-5 xl:col-start-6 xl:col-span-7">
+			<div class="impressum-block col-start-1 col-span-2 md:col-start-2 md:col-span-5 xl:col-start-6 xl:col-span-7 pt-24 md:pt-24">
 				<h2 class="impressum-block__title"><?php the_field( 'credits_title' ); ?></h2>
 				<div class="impressum-block__content"><?php the_field( 'credits_content' ); ?></div>
 			</div>
