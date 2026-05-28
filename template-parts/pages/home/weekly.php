405ba4de7d2e2dc8be0459e16b40917ad3f8c493
bankz91 <brunoduarte.91300@gmail.com>
2026-05-28 16:21:33 +0100

    1

--- template-parts/pages/home/weekly.php
+++ template-parts/pages/home/weekly.php
@@ -10,7 +10,7 @@
 ?>
 <section id="section-weekly" class="section-weekly bg-sand py-[100px] md:py-[100px]" data-weekly-reveal>
 	<div class="section-weekly__border" data-reveal-border>
-		<div class="pt-16 pb-12 md:py-[214px] xl:py-40 text-center">
+		<div class="pt-16 pb-12 md:pt-0 md:pb-0 md:py-20 xl:py-40 text-center">
 			<?php if ( get_field( 'weekly_title' ) ) : ?>
 				<h2 class="title-secondary text-wine max-w-[295px] md:max-w-[587px] xl:max-w-none mx-auto" data-reveal-title><?php the_field( 'weekly_title' ); ?></h2>
 			<?php endif; ?>
